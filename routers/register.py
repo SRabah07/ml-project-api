af268a5a5edd38bf4335fcb1cb548397aa33020f
Salem Aouana <salem.aouana@gmail.com>
2021-12-25 18:15:10 +0100

    add username check availability during registeration

--- routers/register.py
+++ routers/register.py
@@ -1,9 +1,9 @@
 import logging
 from fastapi import APIRouter, HTTPException
 from password_validator import PasswordValidator
-from starlette.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from models import UserCreate
-from db.manage_user import create_user
+from db.manage_user import create_user, get_user
 
 logger = logging.getLogger(__name__)
 
@@ -25,15 +25,27 @@ schema \
 @router.post('/', status_code=HTTP_201_CREATED)
 async def register(user: UserCreate):
     logger.debug(f'Register user')
-    validate(user)
+    await validate(user)
     user_id = await create_user(user)
     logger.debug(f'User with id={user_id} registered')
     return user_id
 
 
-def validate(user: UserCreate):
+async def validate(user: UserCreate):
     if not user:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="User is required!")
+
+    # Check username availability
+    user_db = None
+    try:
+        user_db = await get_user(user.username)
+    except HTTPException as e:
+        if e.status_code != HTTP_404_NOT_FOUND:
+            raise e
+
+    logger.info(f'User = {user_db}')
+    if user_db is not None:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Username not available!")
 
     # Login / Username
     if len(user.username) < MIN_CHARACTERS_USERNAME:
